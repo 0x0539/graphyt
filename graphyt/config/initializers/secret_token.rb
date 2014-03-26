069a5110e9b95148e07955d3c71dd261fe689d2a
Sebastian Goodman <sebastian.goodman@gmail.com>
2014-03-26 00:39:30 -0700

    initial rails project

---
+++ graphyt/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Graphyt::Application.config.secret_key_base = '5781598affafd364d6a1fd1b3f18d4ffc107a0f85a507a7b5b3130e4dc765d219af7a308d2ea1b221ea1953cdbe36ecbb2115c668f786bfbab993cee9eed4f17'
